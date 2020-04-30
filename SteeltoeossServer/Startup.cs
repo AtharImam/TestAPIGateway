6a13326bad0f69e0f54bea440fab2fa7e395399e
Athar Imam <atharandaftab@hotmail.com>
2020-04-30 20:44:27 +0530

    first commit

--- SteeltoeossServer/Startup.cs
+++ SteeltoeossServer/Startup.cs
@@ -36,10 +36,7 @@ namespace SteeltoeossServer
             app.UseDiscoveryClient();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
